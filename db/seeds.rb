9d11dba7a1067b26ec94ea16ff7cf89ebfccc72f
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-05-14 15:55:19 +1000

    completed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Course.destroy_all
+User.destroy_all
 
-user = User.new(first_name: 'user', last_name: 'tester', email:'test@test.com', password: 'password')
+admin = User.create(first_name: 'admin', last_name: 'tester', email:'admin@test.com', password: 'password', is_admin?: true)
+mentor_1 = User.create(first_name: 'Elon', last_name: 'Tusk', email:'mentor_1@test.com', password: 'password', is_mentor?: true)
+mentor_2 = User.create(first_name: 'Rick', last_name: 'Ricks', email:'mentor_2@test.com', password: 'password', is_mentor?: true)
+user_1 = User.create(first_name: 'Morty', last_name: 'Clone', email:'user_1@test.com', password: 'password')
+
+mentor_1.create_mentor(description: 'A successful Tusk', expertise: 'Expertise', business: 'Business', industry: 'Industry')
+
+mentor_2.create_mentor(description: 'A successful grumpy scientist', expertise: 'Expertise', business: 'Business', industry: 'Industry')
+
+10.times do Course.create(
+  title: Faker::Educator.subject, 
+  sub_title: Faker::Educator.course_name, 
+  price: rand(100..5000), 
+  description: Faker::Marketing.buzzwords,
+  mentor_id: Mentor.all.sample.id
+)
+end
+
