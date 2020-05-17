427e93dfa47871fb91d13d036a3edfebad47560e
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-05-17 20:31:51 +1000

    added banner and moved partials into shared dir

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,18 @@
 class CartsController < ApplicationController
+
+  def index
+    if user_signed_in? && current_user.cart && current_user.is_student?
+      @cart = current_user.cart.courses
+    else  
+      redirect_to courses_path
+    end
+  end
+
+  def create # add course into cart 
+    if current_user.is_student? && !current_user.cart
+      @cart = current_user.create_cart
+    end
+  end
+
+
 end
